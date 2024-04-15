7cc5479bb627e63ce7e476810393fe9123150d22
Atem89 <bortnic.eugen@profesor.cihcahul.md>
2024-04-15 12:08:46 +0300

    Initial Commit

---
+++ src/main/java/homework_nr_9/Erbivor.java
@@ -0,0 +1,11 @@
+package homework_nr_9;
+
+public abstract class Erbivor{
+
+    public abstract void food();
+
+    public void whatIsHeDoing(){
+        System.out.println("The herbivore is currently sleep");
+    }
+
+}
