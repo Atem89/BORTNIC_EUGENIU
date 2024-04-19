bd7c332e1cd9140c229909bb3c869e682a29b24a
Atem89 <bortnic.eugen@profesor.cihcahul.md>
2024-04-19 11:49:18 +0300

    merge method in Main class

--- src/main/java/homework_nr_8/Main.java
+++ src/main/java/homework_nr_8/Main.java
@@ -1,27 +1,35 @@
 package homework_nr_8;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ToDoList toDo = new ToDoList();
-        List<String> items = toDo.getToDoList();
+        List<String> listToday = new ArrayList<>();
+        listToday.add("Wake up");
+        listToday.add("Light exercise");
+        listToday.add("Have breakfast");
+        listToday.add("Work");
+        listToday.add("Study");
+        listToday.add("Wake up");
+        listToday.add("Light exercise");
+        listToday.add("Have breakfast");
+        listToday.add("Work");
+        listToday.add("Study");
 
-        for (String item : items) {
-            System.out.println(" - " + item);
+        for (String item : listToday){
+            System.out.println("- " + item);
         }
-
         System.out.println("--------------------------------------------------------------------");
-        NoDuplicate listOriginal = new NoDuplicate();
-        List<String> original = listOriginal.noDuplicate(items);
-
-        System.out.println(" - " + original);
-
-        System.out.println("--------------------------------------------------------------------");
-
+        printOriginalList(listToday);
     }
+
+
+    public static void printOriginalList(List<String> listToday){
+        Set<String> originalList = new HashSet<>(listToday);
+        System.out.println(originalList);
+    }
+
+
 }
